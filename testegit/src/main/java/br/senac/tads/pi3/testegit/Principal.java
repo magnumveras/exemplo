a9dabc95c90f3bc1585ffc22542360ea1a13ecf8
magno.mveras <magno.mveras@CAS511778W0734.senacsp.edu.br>
2017-02-21 21:25:02 -0300

    Mostrar somente se i = par

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -14,7 +14,11 @@ public class Principal {
     public static void main(String[] args) {
         
         for(int i = 0; i < 10; i++){
-            System.out.println("Hello World: " + (i+1));
+            if(i % 2 == 0){
+               System.out.println("Hello World: " + i); 
+            }
+               
+            
         }
         
     }
