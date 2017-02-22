d0bfaa9a163da85193c88c1e501fed3f7e53aad0
magno.mveras <magno.mveras@CAS511778W0734.senacsp.edu.br>
2017-02-21 21:27:15 -0300

    Mostrar somente se i = ímpar

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -14,7 +14,8 @@ public class Principal {
     public static void main(String[] args) {
         
         for(int i = 0; i < 10; i++){
-            System.out.println("Hello World: " + (i+1));
+            if(i % 2 == 1)
+            System.out.println("Hello World: " + i);
         }
         
     }
